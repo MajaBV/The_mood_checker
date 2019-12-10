a02237491b56c2b9e0183ba683163ebfe86234a7
Maja Vukadin <maja.barisic5@gmail.com>
2019-12-10 12:40:20 +0100

    initial commit

---
+++ The_mood_checker.py
@@ -0,0 +1,14 @@
+mood = input("What is your mood at the moment? ")
+
+if mood == "happy":
+    print("It is great to see you happy!")
+elif mood == "nervous":
+    print("Take a deep breath 3 times.")
+elif mood == "sad":
+    print("Don't be so, you have many more opportunities.")
+elif mood == "excited":
+    print("Where are you going again?")
+elif mood == "relaxed":
+    print("Sounds great.")
+else:
+    print("I don't recognize this mood.")
